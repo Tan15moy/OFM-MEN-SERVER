02179568793ea1da6923ac796a6e35e96a77e1f5
Tanmoy <tanmoyrana1998@gmail.com>
2020-07-25 16:49:30 +0530

    Database name chnaged

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const url = "mongodb+srv://Admin:Admin123@ofm-server.oowbq.mongodb.net/football_manager?retryWrites=true&w=majority"
-
 const app = express();
 mongoose.connect(url , { useNewUrlParser: true, useUnifiedTopology: true })
 const con = mongoose.connection
@@ -15,6 +14,10 @@ const port = process.env.PORT || 9000
 const host ='0.0.0.0'
 app.listen(port,host, () =>{
     console.log("Server running...on port: "+port)
+})
+
+app.get('/', (req,res) =>{
+    res.send("Succesfull")
 })
 
 const playersRouter = require('./routes/players')
