b2de9f8ce9ca62ce88b8b40bd4ba5f2029a68602
Tanmoy <tanmoyrana1998@gmail.com>
2020-07-25 16:30:14 +0530

    Ip changes to all

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const mongoose = require('mongoose')
 const url = "mongodb+srv://Admin:Admin123@ofm-server.oowbq.mongodb.net/ofm-data?retryWrites=true&w=majority"
 
 const app = express();
-mongoose.connect(url , { useNewUrlParser: true })
+mongoose.connect(url , { useNewUrlParser: true, useUnifiedTopology: true })
 const con = mongoose.connection
 
 con.on('open', () => {
-    console.log("connected...")
+    console.log("Mongodb Atlas connected...")
 })
 app.use(express.json())
-app.listen(9000, () =>{
-    console.log("running...")
+
+const port = process.env.PORT || 9000
+const host ='0.0.0.0'
+app.listen(port,host, () =>{
+    console.log("Server running...on port: "+port)
 })
 
 const playersRouter = require('./routes/players')
